95da225bb6edbdef8f8972de99552daaea1eb19a
onmedicines <semwalanurag26@gmail.com>
2024-10-18 16:36:17 +0530

    Check the closest bride(if exists) and print the output.

--- bride.c
+++ bride.c
@@ -4,6 +4,7 @@ struct Bride {
   int x;
   int y;
   int numberOfNeighbours;
+  int distanceFromBoy;
 };
 
 // calculate number of neighbors for a given bride
@@ -25,7 +26,6 @@ int helperFunction(int skipValueForI, int skipValueForJ, int* size,
 
 int bridesInNeighbourhood(int* size, int matrix[size[0]][size[1]], int x,
   int y) {
-
   int rows = size[0], cols = size[1];
   int numberOfNeighbours = 0;
   if (x != 0 && y != 0 && x != (rows - 1) && y != (cols - 1)) {
@@ -52,40 +52,26 @@ int bridesInNeighbourhood(int* size, int matrix[size[0]][size[1]], int x,
   else if (y == (cols - 1)) {
     numberOfNeighbours = helperFunction(10, 1, size, matrix, x, y);
   }
-
   return numberOfNeighbours;
-}
-
-// calculate number of brides with maximum number of attributes
-int calculateContendersForWife(struct Bride* brides, int length, int max) {
-  int contenders = 0;
-  for (int i = 0; i < length; i++) {
-    if (brides[i].numberOfNeighbours == max) {
-      contenders++;
-    }
-  }
-  return contenders;
 }
 
 void main() {
   int size[2];
 
   printf("Please enter the size of the matrix.\n");
-  printf("Rows: ");
-  scanf("%d", &size[0]);
-  printf("Columns: ");
-  scanf("%d", &size[1]);
+  // printf("Rows: ");
+  // scanf("%d", &size[0]);
+  // printf("Columns: ");
+  // scanf("%d", &size[1]);
+  scanf("%d %d", &size[0], &size[1]);
 
   int matrix[size[0]][size[1]];
 
   printf("Please enter the matrix elements.\n");
-  printf("The format would be assumed as follows:\n");
-
   int numberOfBrides = 0;
 
   for (int i = 0; i < size[0]; i++) {
     for (int j = 0; j < size[1]; j++) {
-      printf("\n>> ");
       scanf("%d", &matrix[i][j]);
       if (matrix[i][j] == 1)
         numberOfBrides++;
@@ -93,12 +79,6 @@ void main() {
   }
 
   struct Bride brides[numberOfBrides];
-
-  for (int i = 0; i < numberOfBrides; i++) {
-    brides[i].x = 0;
-    brides[i].y = 0;
-    brides[i].numberOfNeighbours = 0;
-  }
 
   int index = 0;
   int maxNeighbours = 0;
@@ -115,11 +95,61 @@ void main() {
       }
     }
   }
+  index = 0;
 
+  struct Bride favourableBride;
+  favourableBride.distanceFromBoy = 0;
+  int numberOfBridesWithMaxQualities = 0;
   for (int i = 0; i < numberOfBrides; i++) {
-    printf("Bride %d:", i);
-    printf("x: %d, y: %d, number of neighbours: %d\n", brides[i].x, brides[i].y,
-      brides[i].numberOfNeighbours);
+    if (brides[i].numberOfNeighbours == maxNeighbours) {
+      numberOfBridesWithMaxQualities++;
+      favourableBride = brides[i];
+    }
   }
 
+  // if there is only one girl with most qualities she is the bride
+  // hence display her details and end program
+  if (numberOfBridesWithMaxQualities == 1) {
+    printf("%d:%d:%d\n", favourableBride.x + 1, favourableBride.y + 1,
+      favourableBride.numberOfNeighbours);
+    return;
+  }
+
+  index = 0;
+  struct Bride bridesWithMaxQualities[numberOfBridesWithMaxQualities];
+  int minDistanceFromBoy = size[0] + size[1];
+  for (int i = 0; i < numberOfBrides; i++) {
+    if (brides[i].numberOfNeighbours == maxNeighbours) {
+      bridesWithMaxQualities[index] = brides[i];
+      bridesWithMaxQualities[index].distanceFromBoy = brides[i].x + brides[i].y;
+      if (bridesWithMaxQualities[index].distanceFromBoy < minDistanceFromBoy) {
+        minDistanceFromBoy = bridesWithMaxQualities[index].distanceFromBoy;
+      }
+      index++;
+    }
+  }
+
+  int numberOfFavourableBrides = 0;
+  for (int i = 0; i < numberOfBridesWithMaxQualities; i++) {
+    if (bridesWithMaxQualities[i].distanceFromBoy == minDistanceFromBoy) {
+      numberOfFavourableBrides++;
+      favourableBride = bridesWithMaxQualities[i];
+    }
+  }
+
+  if (numberOfFavourableBrides > 1) {
+    printf("Polygamy not allowed!\n");
+    return;
+  }
+
+  // for (int i = 0; i < numberOfBridesWithMaxQualities; i++) {
+  //   if (bridesWithMaxQualities[i].distanceFromBoy == minDistanceFromBoy) {
+  //     favourableBride = bridesWithMaxQualities[i];
+  //   }
+  // }
+
+
+  printf("%d:%d:%d\n", favourableBride.x + 1, favourableBride.y + 1,
+    favourableBride.numberOfNeighbours);
+  return;
 }
